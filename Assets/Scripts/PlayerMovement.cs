bde16132590eb028bbc37ff51092ff4a6c6e32aa
Hermit-commits-code <179908131+Hermit-commits-code@users.noreply.github.com>
2024-09-21 17:30:47 -0400

    Added sideways force and forward force

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
     // Start is called before the first frame update
 
     // I marked this as FixedUpdate because I'm testing the physics.
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 200 * Time.deltaTime);
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            // Only executed if condition is met.
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            // Only executed if condition is met.
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
