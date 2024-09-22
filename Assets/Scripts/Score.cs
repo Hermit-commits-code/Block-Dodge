ee83c3d59c9f74caf9422a934dece76e981e8082
Hermit-commits-code <179908131+Hermit-commits-code@users.noreply.github.com>
2024-09-22 14:58:39 -0400

    Got the score UI to work using Score.cs

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,13 +1,15 @@
-using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-    public Text scoreText;
+    public TMP_Text scoreText;
     public Transform player;
+
     // Update is called once per frame
     void Update()
     {
+        // Debug.Log(player.position.z);
         scoreText.text = player.position.z.ToString("0");
     }
 }
