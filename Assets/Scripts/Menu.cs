4c680be51897d5010cada5f37ca87e3d3cad45d2
Hermit-commits-code <179908131+Hermit-commits-code@users.noreply.github.com>
2024-09-23 18:28:29 -0400

    Finished the changes for Version 1.0

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,9 +1,7 @@
-using UnityEngine.SceneManagement;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
